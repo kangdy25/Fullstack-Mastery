dafcbe7b5dc7160fb52fab5f640626432aecc36f
Shineast <canfrog@naver.com>
2025-12-11 23:56:31 +0900

    Introducing TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return <div className="text-3xl font-bold text-sky-400">Hello World</div>;
 }
 
 export default App;
