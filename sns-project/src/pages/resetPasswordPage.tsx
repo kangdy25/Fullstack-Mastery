6ce9d630d0117afdf0924cc67757239f6b6b0709
Kangdy <canfrog@naver.com>
2026-01-05 22:47:41 +0900

    Implement ResetPasswordPage UI

--- sns-project/src/pages/resetPasswordPage.tsx
+++ sns-project/src/pages/resetPasswordPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const resetPasswordPage = () => {
-  return <div>resetPasswordPage</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">비밀번호 재설정하기</div>
+        <div className="text-muted-foreground">
+          새로운 비밀번호를 입력하세요
+        </div>
+      </div>
+      <Input type="password" className="py-6" placeholder="password" />
+      <Button className="w-full">비밀번호 변경하기</Button>
+    </div>
+  );
 };
 
 export default resetPasswordPage;
