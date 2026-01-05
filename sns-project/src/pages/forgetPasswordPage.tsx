46f32da9b94ac293c7d7bd8bd8e9fef9daa7a8f1
Kangdy <canfrog@naver.com>
2026-01-05 22:45:04 +0900

    Implement ForgetPasswordPage UI

--- sns-project/src/pages/forgetPasswordPage.tsx
+++ sns-project/src/pages/forgetPasswordPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const forgetPasswordPage = () => {
-  return <div>forgetPasswordPage</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">비밀번호를 잊으셨나요?</div>
+        <div className="text-muted-foreground">
+          이메일로 비밀번호를 재설정할 수 있는 인증 링크를 보내드립니다.
+        </div>
+      </div>
+      <Input className="py-6" placeholder="example@abc.com" />
+      <Button className="w-full">인증 메일 요청하기</Button>
+    </div>
+  );
 };
 
 export default forgetPasswordPage;
