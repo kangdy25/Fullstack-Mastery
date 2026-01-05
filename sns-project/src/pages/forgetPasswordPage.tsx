c3d9b9b7cdacf375a10aa99ecebe9f5fff25d894
Kangdy <canfrog@naver.com>
2026-01-05 23:08:12 +0900

    Implement useRequestPasswordResetEmail Function

--- sns-project/src/pages/forgetPasswordPage.tsx
+++ sns-project/src/pages/forgetPasswordPage.tsx
@@ -1,7 +1,37 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useRequestPasswordResetEmail } from "@/hooks/mutations/useRequestPasswordResetEmail";
+import { generateErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const forgetPasswordPage = () => {
+  const [email, setEmail] = useState("");
+
+  const {
+    mutate: requestPasswordResetEmail,
+    isPending: isRequestPasswordResetEmailPending,
+  } = useRequestPasswordResetEmail({
+    onSuccess: () => {
+      toast.info("인증 메일이 잘 발송되었습니다.", {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+    onError: (error) => {
+      const message = generateErrorMessage(error);
+      toast.error(message, {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+  });
+
+  const handleSendEmailClick = () => {
+    if (email.trim() === "") return;
+    requestPasswordResetEmail(email);
+  };
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-1">
@@ -10,8 +40,20 @@ const forgetPasswordPage = () => {
           이메일로 비밀번호를 재설정할 수 있는 인증 링크를 보내드립니다.
         </div>
       </div>
-      <Input className="py-6" placeholder="example@abc.com" />
-      <Button className="w-full">인증 메일 요청하기</Button>
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isRequestPasswordResetEmailPending}
+        className="py-6"
+        placeholder="example@abc.com"
+      />
+      <Button
+        onClick={handleSendEmailClick}
+        disabled={isRequestPasswordResetEmailPending}
+        className="w-full"
+      >
+        인증 메일 요청하기
+      </Button>
     </div>
   );
 };
