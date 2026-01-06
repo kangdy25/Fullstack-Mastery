674b32fce9251e85da23160cdc8d86e0f6a06e70
Kangdy <canfrog@naver.com>
2026-01-06 19:59:08 +0900

    Implement profile viewing

--- sns-project/src/provider/sessionProvider.tsx
+++ sns-project/src/provider/sessionProvider.tsx
@@ -1,12 +1,18 @@
 import { type ReactNode } from "react";
 import { useEffect } from "react";
 import supabase from "@/utils/supabase";
-import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useIsSessionLoaded, useSession, useSetSession } from "@/store/session";
 import GlobalLoader from "@/components/globalLoader";
+import { useProfileData } from "@/hooks/queries/useProfileData";
 
 const SessionProvider = ({ children }: { children: ReactNode }) => {
+  const session = useSession();
   const setSession = useSetSession();
   const isSessionLoaded = useIsSessionLoaded();
+
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -15,6 +21,7 @@ const SessionProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   if (!isSessionLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />;
 
   return children;
 };
