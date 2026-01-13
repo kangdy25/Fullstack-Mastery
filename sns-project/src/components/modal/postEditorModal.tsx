4ad3f6b1e4b97cb6ea095546bf267eadfe744e7f
Kangdy <canfrog@naver.com>
2026-01-13 22:48:48 +0900

    Implement Image Upload (2)

--- sns-project/src/components/modal/postEditorModal.tsx
+++ sns-project/src/components/modal/postEditorModal.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import { useCreatePost } from "@/hooks/mutations/post/useCreatePost";
 import { toast } from "sonner";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { useSession } from "@/store/session";
 
 type Image = {
   file: File;
@@ -13,6 +14,7 @@ type Image = {
 };
 
 const PostEditorModal = () => {
+  const session = useSession();
   const { isOpen, close } = usePostEditorModal();
   const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
     onSuccess: () => {
@@ -36,7 +38,11 @@ const PostEditorModal = () => {
 
   const handleCreatePostClick = () => {
     if (content.trim() === "") return;
-    createPost(content);
+    createPost({
+      content,
+      images: images.map((image) => image.file),
+      userId: session!.user.id,
+    });
   };
 
   const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
