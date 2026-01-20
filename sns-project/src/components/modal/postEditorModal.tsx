6f0e12667ac10b9f0a707797108e66f2ea04f744
Kangdy <canfrog@naver.com>
2026-01-20 20:17:07 +0900

    Implement post editing functionality

--- sns-project/src/components/modal/postEditorModal.tsx
+++ sns-project/src/components/modal/postEditorModal.tsx
@@ -8,6 +8,7 @@ import { toast } from "sonner";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import { useSession } from "@/store/session";
 import { useOpenAlertModal } from "@/store/alertModal";
+import { useUpdatePost } from "@/hooks/mutations/post/useUpdatePost";
 
 type Image = {
   file: File;
@@ -17,10 +18,11 @@ type Image = {
 const PostEditorModal = () => {
   const session = useSession();
   const openAlertModal = useOpenAlertModal();
-  const { isOpen, close } = usePostEditorModal();
+  const postEditorModal = usePostEditorModal();
+
   const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
     onSuccess: () => {
-      close();
+      postEditorModal.actions.close();
     },
     onError: (error) => {
       toast.error("포스트 생성에 실패했습니다.", {
@@ -28,6 +30,18 @@ const PostEditorModal = () => {
       });
     },
   });
+
+  const { mutate: updatePost, isPending: isUpdatePostPending } = useUpdatePost({
+    onSuccess: () => {
+      postEditorModal.actions.close();
+    },
+    onError: (error) => {
+      toast.error("포스트 수정에 실패했습니다.", {
+        position: "top-center",
+      });
+    },
+  });
+
   const [content, setContent] = useState("");
   const [images, setImages] = useState<Image[]>([]);
 
@@ -43,16 +57,23 @@ const PostEditorModal = () => {
   }, [content]);
 
   useEffect(() => {
-    if (!isOpen) {
+    if (!postEditorModal.isOpen) {
       images.forEach((image) => {
         URL.revokeObjectURL(image.previewUrl);
       });
       return;
     }
+
+    if (postEditorModal.type === "CREATE") {
+      setContent("");
+      setImages([]);
+    } else {
+      setContent(postEditorModal.content);
+      setImages([]);
+    }
+
     textareaRef.current?.focus();
-    setContent("");
-    setImages([]);
-  }, [isOpen]);
+  }, [postEditorModal.isOpen]);
 
   const handleCloseModal = () => {
     if (content !== "" || images.length !== 0) {
@@ -60,21 +81,31 @@ const PostEditorModal = () => {
         title: "게시글 작성이 마무리되지 않았습니다.",
         description: "이 화면에서 나가면 작성 중이던 내용이 사라집니다.",
         onPositive: () => {
-          close();
+          postEditorModal.actions.close();
         },
       });
       return;
     }
-    close();
+    postEditorModal.actions.close();
   };
 
-  const handleCreatePostClick = () => {
+  const handleSavePostClick = () => {
     if (content.trim() === "") return;
-    createPost({
-      content,
-      images: images.map((image) => image.file),
-      userId: session!.user.id,
-    });
+    if (!postEditorModal.isOpen) return;
+
+    if (postEditorModal.type === "CREATE") {
+      createPost({
+        content,
+        images: images.map((image) => image.file),
+        userId: session!.user.id,
+      });
+    } else {
+      if (content === postEditorModal.content) return;
+      updatePost({
+        id: postEditorModal.postId,
+        content: content,
+      });
+    }
   };
 
   const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
@@ -99,12 +130,14 @@ const PostEditorModal = () => {
     URL.revokeObjectURL(image.previewUrl);
   };
 
+  const isPending = isCreatePostPending || isUpdatePostPending;
+
   return (
-    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
+    <Dialog open={postEditorModal.isOpen} onOpenChange={handleCloseModal}>
       <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
         <textarea
-          disabled={isCreatePostPending}
+          disabled={isPending}
           ref={textareaRef}
           value={content}
           onChange={(e) => setContent(e.target.value)}
@@ -119,6 +152,23 @@ const PostEditorModal = () => {
           className="hidden"
           onChange={handleSelectImages}
         />
+        {postEditorModal.isOpen && postEditorModal.type === "EDIT" && (
+          <Carousel>
+            <CarouselContent>
+              {postEditorModal.imageUrls?.map((url) => (
+                <CarouselItem key={url} className="basis-2/5">
+                  <div className="relative">
+                    <img
+                      src={url}
+                      alt=""
+                      className="h-full w-full rounded-sm object-cover"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        )}
         {images.length > 0 && (
           <Carousel>
             <CarouselContent>
@@ -142,20 +192,22 @@ const PostEditorModal = () => {
             </CarouselContent>
           </Carousel>
         )}
+        {postEditorModal.isOpen && postEditorModal.type === "CREATE" && (
+          <Button
+            onClick={() => {
+              fileInputRef.current?.click();
+            }}
+            disabled={isPending}
+            variant={"outline"}
+            className="cursor-pointer"
+          >
+            <ImageIcon />
+            이미지 추가
+          </Button>
+        )}
         <Button
-          onClick={() => {
-            fileInputRef.current?.click();
-          }}
-          disabled={isCreatePostPending}
-          variant={"outline"}
-          className="cursor-pointer"
-        >
-          <ImageIcon />
-          이미지 추가
-        </Button>
-        <Button
-          disabled={isCreatePostPending}
-          onClick={handleCreatePostClick}
+          disabled={isPending}
+          onClick={handleSavePostClick}
           className="cursor-pointer"
         >
           저장
---
+++ sns-project/src/components/post/editPostItemButton.tsx
@@ -0,0 +1,27 @@
+import { useOpenEditPostModal } from "@/store/postEditorModal";
+import { Button } from "../ui/button";
+import type { PostEntity } from "@/types";
+
+const EditPostItemButton = (props: PostEntity) => {
+  const openEditPostModal = useOpenEditPostModal();
+
+  const handleButtonClick = () => {
+    openEditPostModal({
+      postId: props.id,
+      content: props.content,
+      imageUrls: props["image-urls"],
+    });
+  };
+
+  return (
+    <Button
+      onClick={handleButtonClick}
+      className="cursor-pointer"
+      variant={"ghost"}
+    >
+      수정
+    </Button>
+  );
+};
+
+export default EditPostItemButton;
