ca3fb1f00845dbaf42d4a1d0488812d7b4a9d205
Kangdy <canfrog@naver.com>
2026-02-16 23:42:49 +0900

    Implement Comment UI

--- sns-project/src/pages/postDetailPage.tsx
+++ sns-project/src/pages/postDetailPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import CommentEditor from "@/components/comment/commentEditor";
+import CommentList from "@/components/comment/commentList";
+import PostItem from "@/components/post/postItem";
+import { Navigate, useParams } from "react-router";
 
 const postDetailPage = () => {
-  return <div>postDetailPage</div>;
+  const params = useParams();
+  const postId = params.postId;
+
+  if (!postId) return <Navigate to={"/"} />;
+
+  return (
+    <div>
+      <PostItem postId={Number(postId)} type="DETAIL" />
+      <div className="text-xl font-bold">댓글</div>
+      <CommentEditor />
+      <CommentList />
+    </div>
+  );
 };
 
 export default postDetailPage;
